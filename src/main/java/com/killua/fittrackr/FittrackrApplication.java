ef0f9359d553724e7af321ba2c5565a0a8d82483
andreizipistopgap <andrei.zipis8@gmail.com>
2026-05-04 17:09:02 +0300

    Initial Spring Boot project setup

---
+++ src/main/java/com/killua/fittrackr/FittrackrApplication.java
@@ -0,0 +1,13 @@
+package com.killua.fittrackr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FittrackrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FittrackrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/killua/fittrackr/FittrackrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.killua.fittrackr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FittrackrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
